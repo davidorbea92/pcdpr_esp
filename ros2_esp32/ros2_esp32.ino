8abe6b9bdcb7b274333d0998f78c51829d2fa0fd
David Orbea Jerez <95913372+davidorbea92@users.noreply.github.com>
2024-09-09 13:29:17 +0200

    Update

--- ros2_esp32/ros2_esp32.ino
+++ ros2_esp32/ros2_esp32.ino
@@ -1,29 +1,52 @@
-/*Arduino program to control speed of RMD X6-S2 motor through /cmd_vel topic
- Developed by: David Orbea
- Institution: Universidad Politecnica de Madrid
- February 2024
-*/
+#include <micro_ros_arduino.h>
 
-#include <ros.h>
-#include <geometry_msgs/Twist.h>
+#include <stdio.h>
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+#include <geometry_msgs/msg/twist.h>
+
 #include <mcp_can.h>
 
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
+
+#define LED_PIN 2
+
+// ROS2 DECLARATIONS
+rcl_publisher_t pub_pos;
+rcl_publisher_t pub_speed;
+
+rcl_subscription_t m1s;
+
+geometry_msgs__msg__Twist msg;
+geometry_msgs__msg__Twist msg_pub_pos;
+geometry_msgs__msg__Twist msg_pub_speed;
+
+rclc_executor_t executor;
+rcl_allocator_t allocator;
+rclc_support_t support;
+rcl_node_t node;
+
+//variables de cdpr
 //Motor id
-long motor1_id=0x141;
-long motor2_id=0x142;
-long motor3_id=0x143;
-long motor4_id=0x144;
+float motor1_id=0x141;
+float motor2_id=0x142;
+float motor3_id=0x143;
+float motor4_id=0x144;
 
 //Motor id for receiving
-long motor1receive_id=0x241;
-long motor2receive_id=0x242;
-long motor3receive_id=0x243;
-long motor4receive_id=0x244;
+float motor1receive_id=0x241;
+float motor2receive_id=0x242;
+float motor3receive_id=0x243;
+float motor4receive_id=0x244;
 
 //for position control
 unsigned long startMillis=0;
 unsigned long currentMillis=0;
-unsigned long period=100; //period of position publishing in milliseconds
+unsigned long period=50; //period of position publishing in milliseconds
 
 //variables for position response
 uint8_t command;
@@ -58,15 +81,13 @@ unsigned char rxBuf[8];
 long speedControl=0;
 long posControl=0;
 
-long speedControlconstant=5729.577951308;
+float speedControlconstant=5729.577951308;
 float posControlconstant=5729.577951308;
-long speedZero=0;
 
-long posicion1;
-long posicion2;
-long posicion3;
-long posicion4;
-long GenPos=0;
+long posicion1=0;
+long posicion2=0;
+long posicion3=0;
+long posicion4=0;
 
 unsigned char len=0;
 unsigned char buf_speed[8];
@@ -81,20 +102,14 @@ byte datac[8] = {0x88, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 //Pin declaration of MCP module
 MCP_CAN CAN0(5);     // Set CS to pin 10 (arduino) and 15 (esp8266) and 5 (esp32)
 
-//ROS DECLARATIONS
-ros::NodeHandle  nh;
+void error_loop(){
+  while(1){
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+    delay(100);
+  }
+}
 
-geometry_msgs::Twist msg_sub_speed;
-geometry_msgs::Twist msg_sub_pos;
-
-geometry_msgs::Twist msg_pub_pos;
-geometry_msgs::Twist msg_pub_speed;
-
-ros::Publisher pub_speed("motors_speed", &msg_pub_speed);
-ros::Publisher pub_pos("motors_pos", &msg_pub_pos);
-
-
-void send_speed(long motor_id, long motorreceive_id, float vel){
+void send_speed(float motor_id, float motorreceive_id, float vel){
   speedControl=vel*speedControlconstant;
   buf_speed[0]=0xA2;
   buf_speed[1]=0x00;
@@ -105,76 +120,12 @@ void send_speed(long motor_id, long motorreceive_id, float vel){
   buf_speed[6]=(speedControl>>16) & 0xff;
   buf_speed[7]=(speedControl>>24) & 0xff;
   
-  for (int i=0; i<5; i++){
+  for (int i=0; i<2; i++){
     CAN0.sendMsgBuf(motor_id, 0, 8, buf_speed);
   }
 }
 
-void send_position(long motor_id, long motorreceive_id, float pos){
-  posControl=pos*posControlconstant;
-  buf_speed[0]=0xA4;
-  buf_speed[1]=0x00;
-  buf_speed[2]=0x64;
-  buf_speed[3]=0x00;
-  buf_speed[4]=(posControl & 0xFF);
-  buf_speed[5]=(posControl>>8) & 0xff;
-  buf_speed[6]=(posControl>>16) & 0xff;
-  buf_speed[7]=(posControl>>24) & 0xff;
-  
-  for (int i=0; i<5; i++){
-    CAN0.sendMsgBuf(motor_id, 0, 8, buf_speed);
-  }
-}
-
-void callbackm1_s(const geometry_msgs::Twist& msg_sub){
-  float cmd_ros=msg_sub.angular.z;
-  send_speed(motor1_id, motor1receive_id, cmd_ros);
-}
-void callbackm2_s(const geometry_msgs::Twist& msg_sub){
-  float cmd_ros=msg_sub.angular.z;
-  send_speed(motor2_id, motor2receive_id, cmd_ros);
-}
-
-void callbackm3_s(const geometry_msgs::Twist& msg_sub){
-  float cmd_ros=msg_sub.angular.z;
-  send_speed(motor3_id, motor3receive_id, cmd_ros);
-}
-
-void callbackm4_s(const geometry_msgs::Twist& msg_sub){
-  float cmd_ros=msg_sub.angular.z;
-  send_speed(motor4_id, motor4receive_id, cmd_ros);
-}
-
-void callbackm1_pos(const geometry_msgs::Twist& msg_sub){
-  float set_pos=msg_sub.angular.z;
-  send_position(motor1_id, motor1receive_id, set_pos);
-}
-
-void callbackm2_pos(const geometry_msgs::Twist& msg_sub){
-  float set_pos=msg_sub.angular.z;
-  send_position(motor2_id, motor2receive_id, set_pos);
-}
-
-void callbackm3_pos(const geometry_msgs::Twist& msg_sub){
-  float set_pos=msg_sub.angular.z;
-  send_position(motor3_id, motor3receive_id, set_pos);
-}
-
-void callbackm4_pos(const geometry_msgs::Twist& msg_sub){
-  float set_pos=msg_sub.angular.z;
-  send_position(motor4_id, motor4receive_id, set_pos);
-}
-
-ros::Subscriber<geometry_msgs::Twist> m1_s("m1_vel", &callbackm1_s);
-ros::Subscriber<geometry_msgs::Twist> m2_s("m2_vel", &callbackm2_s);
-ros::Subscriber<geometry_msgs::Twist> m3_s("m3_vel", &callbackm3_s);
-ros::Subscriber<geometry_msgs::Twist> m4_s("m4_vel", &callbackm4_s);
-
-ros::Subscriber<geometry_msgs::Twist> m1_pos("m1_pos", &callbackm1_pos);
-ros::Subscriber<geometry_msgs::Twist> m2_pos("m2_pos", &callbackm2_pos);
-ros::Subscriber<geometry_msgs::Twist> m3_pos("m3_pos", &callbackm3_pos);
-ros::Subscriber<geometry_msgs::Twist> m4_pos("m4_pos", &callbackm4_pos);
-
+//funciones para publicar y leer los datos de los motores
 void rmd_poswrite(){
 
   buf_pos[0]=0x92;
@@ -186,7 +137,7 @@ void rmd_poswrite(){
   buf_pos[6]=0x00;
   buf_pos[7]=0x00;
 
-  for (int i=0; i<5; i++){
+  for (int i=0; i<2; i++){
     CAN0.sendMsgBuf(motor1_id, 0, 8, buf_pos);
     CAN0.sendMsgBuf(motor2_id, 0, 8, buf_pos);
     CAN0.sendMsgBuf(motor3_id, 0, 8, buf_pos);
@@ -323,69 +274,91 @@ void rmd_posread(){
   }
 }
 
+//publicacion de la posicion actual en ros2
 void ros_pospub(){
- 
   msg_pub_pos.linear.x=posicion1/posControlconstant;
   msg_pub_pos.linear.y=posicion2/posControlconstant;
   msg_pub_pos.linear.z=posicion3/posControlconstant;
   msg_pub_pos.angular.x=posicion4/posControlconstant;
 
-  pub_pos.publish(&msg_pub_pos);
-
   msg_pub_speed.linear.x=velocidad1;
   msg_pub_speed.linear.y=velocidad2;
   msg_pub_speed.linear.z=velocidad3;
   msg_pub_speed.angular.x=velocidad4;
-  
-  pub_speed.publish(&msg_pub_speed);
+
+  rcl_publish(&pub_pos, &msg_pub_pos, NULL);
+  rcl_publish(&pub_speed, &msg_pub_speed, NULL);
 }
+
+
+//twist message cb
+void ms_callback(const void *msgin) {
+  const geometry_msgs__msg__Twist * msg = (const geometry_msgs__msg__Twist *)msgin;
+  send_speed(motor1_id, motor1receive_id, msg->linear.z);
+  send_speed(motor2_id, motor2receive_id, msg->angular.x);
+  send_speed(motor3_id, motor3receive_id, msg->angular.y);
+  send_speed(motor4_id, motor4receive_id, msg->angular.z);
+}
+
+
 void setup() {
-  //Variables for position publishing
-  startMillis = millis();
+  set_microros_transports();
+
+  allocator = rcl_get_default_allocator();
+
+   //create init_options
+  rclc_support_init(&support, 0, NULL, &allocator);
+
+  // create node
+  rclc_node_init_default(&node, "micro_ros_cdpr_simple", "", &support);
+
+  // create subscriber m1s
+  RCCHECK(rclc_subscription_init_best_effort(
+    &m1s,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
+    "m_vel"));
+
+  // create publisher motor_pos
+  RCCHECK(rclc_publisher_init_best_effort(
+    &pub_pos,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
+    "motors_pos"));
   
+  // create publisher motor_speed
+  RCCHECK(rclc_publisher_init_best_effort(
+     &pub_speed,
+     &node,
+     ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
+     "motors_speed"));
+
+  // create executor
+  rclc_executor_init(&executor, &support.context, 1, &allocator);
+  rclc_executor_add_subscription(&executor, &m1s, &msg, &ms_callback, ON_NEW_DATA);
+
   // Initialize MCP2515 running at 8MHz with a baudrate of 1000kb/s and the masks and filters disabled.
   if(CAN0.begin(MCP_ANY, CAN_1000KBPS, MCP_8MHZ) == CAN_OK)
   CAN0.setMode(MCP_NORMAL);   // Change to normal mode to allow messages to be transmitted
   pinMode(CAN0_INT, INPUT);
 
   //Send first message to turn motor working mode on
-  for (int i=0; i<10; i++){
+  for (int i=0; i<20; i++){
     CAN0.sendMsgBuf(motor1_id, 0, 8, datac);
     CAN0.sendMsgBuf(motor2_id, 0, 8, datac);
     CAN0.sendMsgBuf(motor3_id, 0, 8, datac);
     CAN0.sendMsgBuf(motor4_id, 0, 8, datac);
   }
-
-  //Advertising of node, publishers and subscribers in ROS
-  nh.initNode();
-  nh.subscribe(m1_s);
-  nh.subscribe(m2_s);
-  nh.subscribe(m3_s);
-  nh.subscribe(m4_s);
-
-  nh.subscribe(m1_pos);
-  nh.subscribe(m2_pos);
-  nh.subscribe(m3_pos);
-  nh.subscribe(m4_pos);
-  
-  nh.advertise(pub_pos);
-  nh.advertise(pub_speed);
-  
 }
 
 void loop() {
-  nh.spinOnce();
+  delay(0);
+  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
   rmd_poswrite();
   rmd_posread();
-  //For position publish
-  currentMillis=millis();
-  if (currentMillis-startMillis>=period)
-  {
-    
-    ros_pospub();   
-    
-    //Reset of the timer
-    startMillis=currentMillis;
+  currentMillis = millis();
+  if (currentMillis - startMillis >= period) {
+    ros_pospub();
+    startMillis = currentMillis;
   }
-  
 }
